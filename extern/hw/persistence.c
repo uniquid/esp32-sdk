128c3456f47117107e93f96a244b90304146f077
Stefano Saccucci <stefano.saccucci@gmail.com>
2018-06-22 11:30:19 +0200

    update persistence.c and platformio.ini

---
+++ extern/hw/persistence.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "UID_bchainBTC.h"
+
+char *load_tprv(char *privateKey, size_t size)
+{
+    privateKey = NULL;
+    return privateKey;
+}
+void store_tprv(char *privateKey)
+{
+    //privateKey = NULL;
+    return;
+}
+
+void store_contracts(char * name, uint8_t *contracts, size_t size)
+{
+    return;
+}
+
+void load_contracts(cache_buffer ** cache){
+    
+}
